da66d3911d73ae32b375c7cb09273df10d1b6384
Alexandr <Smyasnikov03@mail.ru>
2023-10-08 21:41:10 +0300

    Test defend

--- src/tests/DurakGameTest.java
+++ src/tests/DurakGameTest.java
@@ -58,6 +58,19 @@ public class DurakGameTest {
     }
 
     @Test
+    void testDefend() { 
+        int input = 1;
+
+        List<Card> playerFirstHand = new ArrayList<>(Arrays.asList(new Card("6", "Пики")));
+        List<Card> playerSecondHand = new ArrayList<>(Arrays.asList(new Card("7", "Пики")));
+        List<Card> table = new ArrayList<>(Arrays.asList(new Card("8", "Пики")));
+    
+        game.defend(playerFirstHand, playerSecondHand, table, new Card("9", "Пики"), input);
+    
+        assertTrue(table.isEmpty());
+    }
+
+    @Test
     void testEndGame() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
